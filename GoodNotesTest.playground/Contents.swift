6f22e5966fda081461a2754c237eb5c67149d81b
Yeniel Landestoy <yeniel.landestoy.mindapps@gmail.com>
2023-04-13 22:53:31 +0200

    Solution

--- GoodNotesTest.playground/Contents.swift
+++ GoodNotesTest.playground/Contents.swift
@@ -1,3 +1,35 @@
-import UIKit
+func getRejectedRequests(requests: [String], limitPerSecond: Int) -> [Int] {
+    var rejectedRequests = [Int]()
+    var requestCounts = [String: Int]()
+    var lastTimestamps = [String: Int]()
 
-var greeting = "Hello, playground"
+    for request in requests {
+        let components = request.split(separator: " ")
+        let requestId = Int(components[0])!
+        let ipAddress = String(components[1])
+        let timestamp = Int(components[2])!
+
+        if requestCounts[ipAddress] == nil {
+            requestCounts[ipAddress] = 0
+        }
+        if lastTimestamps[ipAddress] == nil {
+            lastTimestamps[ipAddress] = 0
+        }
+
+        if timestamp - lastTimestamps[ipAddress]! >= 1000 {
+            // The time window has elapsed, reset the request count.
+            requestCounts[ipAddress] = 0
+            lastTimestamps[ipAddress] = timestamp
+        }
+
+        if requestCounts[ipAddress]! < limitPerSecond {
+            // The request is accepted.
+            requestCounts[ipAddress]! += 1
+        } else {
+            // The request is rejected.
+            rejectedRequests.append(requestId)
+        }
+    }
+
+    return rejectedRequests
+}
